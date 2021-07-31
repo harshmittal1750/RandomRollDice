cdc61c05d504d3d9bdb5c268452e0fdf8484c0a7
Harsh Mittal <harshmittal1750@gmail.com>
2021-08-01 00:41:28 +0530

    relaced text with image

--- app/src/main/java/com/example/randomrolldice/MainActivity.kt
+++ app/src/main/java/com/example/randomrolldice/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.randomrolldice
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 /**
@@ -25,17 +25,22 @@ class MainActivity : AppCompatActivity() {
      */
 
     private fun rollDice() {
-        // Create new Dice object with 6 sides and roll it
-
         val dice = Dice(6)
         val diceRoll = dice.roll()
-        // Update the screen with the dice roll
 
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+        val diceImage: ImageView = findViewById(R.id.imageView)
 
-        val resultTextView2: TextView = findViewById(R.id.textView2)
-        resultTextView2.text = diceRoll.toString()
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
+        diceImage.contentDescription = diceRoll.toString()
     }
 }
 
